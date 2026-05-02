0b8618a7e09e7034f190cfbae24621331dada416
AmateurMind <suhail17mohammad@gmail.com>
2026-05-02 13:56:15 +0530

    installed bettersql and solve calendar issue

--- booking/components/ui/calendar.tsx
+++ booking/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { format } from "date-fns"
 import {
   DayPicker,
   getDefaultClassNames,
@@ -198,7 +199,7 @@ function CalendarDayButton({
     <Button
       variant="ghost"
       size="icon"
-      data-day={day.date.toLocaleDateString(locale?.code)}
+      data-day={format(day.date, "yyyy-MM-dd")}
       data-selected-single={
         modifiers.selected &&
         !modifiers.range_start &&
